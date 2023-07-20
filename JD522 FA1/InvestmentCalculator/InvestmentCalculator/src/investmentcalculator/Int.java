8c40652bdbf5e026e9fd9e2057d0537778a1a21c
Zwelihle Masango <115149107+Zwelihle-M@users.noreply.github.com>
2023-07-20 12:27:06 +0200

    Add files via upload

---
+++ JD522 FA1/InvestmentCalculator/InvestmentCalculator/src/investmentcalculator/Int.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package investmentcalculator;
+
+/**
+ *
+ * @author Jurie Boshoff
+ */
+class Int {
+
+    static String toString(int FY) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
